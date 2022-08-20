311566809a3efce7ffa28769d6ba103574c6519b
Ajay Kumar <ajaykumar1512001@gmail.com>
2022-08-20 18:11:09 +0530

    Image word coreected

--- Exponential_Integral.py
+++ Exponential_Integral.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import math
-from PIL import image
+from PIL import Image
 
 st.set_page_config(page_title="Data is OIL", layout="wide")
 
